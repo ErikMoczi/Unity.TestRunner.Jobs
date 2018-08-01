c940c2ec52d32cafdf33f5bde230ad7791210d42
Erik Moczi <32465761+ErikMoczi@users.noreply.github.com>
2018-08-01 22:28:29 +0200

    Add BurstOptions tests

---
+++ Assets/TestCase/BurstOptions/BurstSupportStrictJobParallelFor.cs
@@ -0,0 +1,40 @@
+﻿using TestRunner.Workers;
+using Unity.Burst;
+using Unity.Collections;
+
+namespace TestCase.BurstOptions
+{
+    [BurstCompile(Support = Support.Strict)]
+    public struct BurstSupportStrictJobParallelFor : IJobParallelForExt<float, float>
+    {
+        private NativeArray<float> _data1;
+        private NativeArray<float> _data2;
+
+        public int DataSize { get; set; }
+
+        public NativeArray<float> Data1
+        {
+            get => _data1;
+            set => _data1 = value;
+        }
+
+        public NativeArray<float> Data2
+        {
+            get => _data2;
+            set => _data2 = value;
+        }
+
+        public void Execute(int i)
+        {
+            _data2[i] = _data1[i];
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public void Init()
+        {
+        }
+    }
+}
---
+++ Assets/WorkSpace/BurstOptionsTest.cs
@@ -0,0 +1,131 @@
+﻿using TestCase.BurstOptions;
+using TestRunner;
+using TestRunner.Generator;
+using TestRunner.Generator.Interfaces;
+using TestRunner.Wrappers.Base;
+using TestRunner.Wrappers.Base.Config;
+using Unity.Collections;
+
+namespace WorkSpace
+{
+    public class BurstOptionsTest : SampleGenerator
+    {
+        private const string TestName = nameof(BurstOptionsTest);
+        private const string DataInt1 = "dataInt1";
+        private const string DataFloat1 = "dataFloat1";
+
+        protected override ISampleConfig[] InitSampleConfigs()
+        {
+            return new ISampleConfig[]
+            {
+                new SampleConfig(typeof(int), DataInt1),
+                new SampleConfig(typeof(float), DataFloat1)
+            };
+        }
+
+        protected override IWorkWrapper[] InitWorkWrappers()
+        {
+            return new[]
+            {
+                #region CompileSynchronously
+
+                WorkerTests<int, int>.RunIJob(TestName, new BurstCompileSynchronouslyTrueJob(), GetData<int>(DataInt1),
+                    GetData<int>(DataInt1), new WorkConfigIJob(Allocator.Persistent)),
+                WorkerTests<int, int>.RunIJob(TestName, new BurstCompileSynchronouslyTrueJob(), GetData<int>(DataInt1),
+                    GetData<int>(DataInt1), new WorkConfigIJob(Allocator.Persistent, true)),
+                WorkerTests<int, int>.RunIJob(TestName, new BurstCompileSynchronouslyFalseJob(), GetData<int>(DataInt1),
+                    GetData<int>(DataInt1), new WorkConfigIJob(Allocator.Persistent)),
+                WorkerTests<int, int>.RunIJob(TestName, new BurstCompileSynchronouslyFalseJob(), GetData<int>(DataInt1),
+                    GetData<int>(DataInt1), new WorkConfigIJob(Allocator.Persistent, true)),
+
+
+                WorkerTests<int, int>.RunIJobParallelFor(TestName, new BurstCompileSynchronouslyTrueJobParallelFor(),
+                    GetData<int>(DataInt1), GetData<int>(DataInt1),
+                    new WorkConfigIJobParallelFor(Allocator.Persistent)),
+                WorkerTests<int, int>.RunIJobParallelFor(TestName, new BurstCompileSynchronouslyTrueJobParallelFor(),
+                    GetData<int>(DataInt1), GetData<int>(DataInt1),
+                    new WorkConfigIJobParallelFor(Allocator.Persistent, 64, true)),
+                WorkerTests<int, int>.RunIJobParallelFor(TestName, new BurstCompileSynchronouslyFalseJobParallelFor(),
+                    GetData<int>(DataInt1), GetData<int>(DataInt1),
+                    new WorkConfigIJobParallelFor(Allocator.Persistent)),
+                WorkerTests<int, int>.RunIJobParallelFor(TestName, new BurstCompileSynchronouslyFalseJobParallelFor(),
+                    GetData<int>(DataInt1), GetData<int>(DataInt1),
+                    new WorkConfigIJobParallelFor(Allocator.Persistent, 64, true)),
+
+                #endregion
+
+                #region Support
+
+                WorkerTests<float, float>.RunIJob(TestName, new BurstSupportRelaxedJob(), GetData<float>(DataFloat1),
+                    GetData<float>(DataFloat1), new WorkConfigIJob(Allocator.Persistent)),
+                WorkerTests<float, float>.RunIJob(TestName, new BurstSupportRelaxedJob(), GetData<float>(DataFloat1),
+                    GetData<float>(DataFloat1), new WorkConfigIJob(Allocator.Persistent, true)),
+                WorkerTests<float, float>.RunIJob(TestName, new BurstSupportStrictJob(), GetData<float>(DataFloat1),
+                    GetData<float>(DataFloat1), new WorkConfigIJob(Allocator.Persistent)),
+                WorkerTests<float, float>.RunIJob(TestName, new BurstSupportStrictJob(), GetData<float>(DataFloat1),
+                    GetData<float>(DataFloat1), new WorkConfigIJob(Allocator.Persistent, true)),
+
+                WorkerTests<float, float>.RunIJobParallelFor(TestName, new BurstSupportRelaxedJobParallelFor(),
+                    GetData<float>(DataFloat1), GetData<float>(DataFloat1),
+                    new WorkConfigIJobParallelFor(Allocator.Persistent)),
+                WorkerTests<float, float>.RunIJobParallelFor(TestName, new BurstSupportRelaxedJobParallelFor(),
+                    GetData<float>(DataFloat1), GetData<float>(DataFloat1),
+                    new WorkConfigIJobParallelFor(Allocator.Persistent, 64, true)),
+                WorkerTests<float, float>.RunIJobParallelFor(TestName, new BurstSupportStrictJobParallelFor(),
+                    GetData<float>(DataFloat1), GetData<float>(DataFloat1),
+                    new WorkConfigIJobParallelFor(Allocator.Persistent)),
+                WorkerTests<float, float>.RunIJobParallelFor(TestName, new BurstSupportStrictJobParallelFor(),
+                    GetData<float>(DataFloat1), GetData<float>(DataFloat1),
+                    new WorkConfigIJobParallelFor(Allocator.Persistent, 64, true)),
+
+                #endregion
+
+                #region Accuracy
+
+                WorkerTests<float, float>.RunIJob(TestName, new BurstAccuracyStdJob(), GetData<float>(DataFloat1),
+                    GetData<float>(DataFloat1), new WorkConfigIJob(Allocator.Persistent)),
+                WorkerTests<float, float>.RunIJob(TestName, new BurstAccuracyStdJob(), GetData<float>(DataFloat1),
+                    GetData<float>(DataFloat1), new WorkConfigIJob(Allocator.Persistent, true)),
+                WorkerTests<float, float>.RunIJob(TestName, new BurstAccuracyLowJob(), GetData<float>(DataFloat1),
+                    GetData<float>(DataFloat1), new WorkConfigIJob(Allocator.Persistent)),
+                WorkerTests<float, float>.RunIJob(TestName, new BurstAccuracyLowJob(), GetData<float>(DataFloat1),
+                    GetData<float>(DataFloat1), new WorkConfigIJob(Allocator.Persistent, true)),
+                WorkerTests<float, float>.RunIJob(TestName, new BurstAccuracyMedJob(), GetData<float>(DataFloat1),
+                    GetData<float>(DataFloat1), new WorkConfigIJob(Allocator.Persistent)),
+                WorkerTests<float, float>.RunIJob(TestName, new BurstAccuracyMedJob(), GetData<float>(DataFloat1),
+                    GetData<float>(DataFloat1), new WorkConfigIJob(Allocator.Persistent, true)),
+                WorkerTests<float, float>.RunIJob(TestName, new BurstAccuracyHighJob(), GetData<float>(DataFloat1),
+                    GetData<float>(DataFloat1), new WorkConfigIJob(Allocator.Persistent)),
+                WorkerTests<float, float>.RunIJob(TestName, new BurstAccuracyHighJob(), GetData<float>(DataFloat1),
+                    GetData<float>(DataFloat1), new WorkConfigIJob(Allocator.Persistent, true)),
+
+                WorkerTests<float, float>.RunIJobParallelFor(TestName, new BurstAccuracyStdJobParallelFor(),
+                    GetData<float>(DataFloat1), GetData<float>(DataFloat1),
+                    new WorkConfigIJobParallelFor(Allocator.Persistent)),
+                WorkerTests<float, float>.RunIJobParallelFor(TestName, new BurstAccuracyStdJobParallelFor(),
+                    GetData<float>(DataFloat1), GetData<float>(DataFloat1),
+                    new WorkConfigIJobParallelFor(Allocator.Persistent, 64, true)),
+                WorkerTests<float, float>.RunIJobParallelFor(TestName, new BurstAccuracyLowJobParallelFor(),
+                    GetData<float>(DataFloat1), GetData<float>(DataFloat1),
+                    new WorkConfigIJobParallelFor(Allocator.Persistent)),
+                WorkerTests<float, float>.RunIJobParallelFor(TestName, new BurstAccuracyLowJobParallelFor(),
+                    GetData<float>(DataFloat1), GetData<float>(DataFloat1),
+                    new WorkConfigIJobParallelFor(Allocator.Persistent, 64, true)),
+                WorkerTests<float, float>.RunIJobParallelFor(TestName, new BurstAccuracyMedJobParallelFor(),
+                    GetData<float>(DataFloat1), GetData<float>(DataFloat1),
+                    new WorkConfigIJobParallelFor(Allocator.Persistent)),
+                WorkerTests<float, float>.RunIJobParallelFor(TestName, new BurstAccuracyMedJobParallelFor(),
+                    GetData<float>(DataFloat1), GetData<float>(DataFloat1),
+                    new WorkConfigIJobParallelFor(Allocator.Persistent, 64, true)),
+                WorkerTests<float, float>.RunIJobParallelFor(TestName, new BurstAccuracyHighJobParallelFor(),
+                    GetData<float>(DataFloat1), GetData<float>(DataFloat1),
+                    new WorkConfigIJobParallelFor(Allocator.Persistent)),
+                WorkerTests<float, float>.RunIJobParallelFor(TestName, new BurstAccuracyHighJobParallelFor(),
+                    GetData<float>(DataFloat1), GetData<float>(DataFloat1),
+                    new WorkConfigIJobParallelFor(Allocator.Persistent, 64, true)),
+
+                #endregion
+            };
+        }
+    }
+}
