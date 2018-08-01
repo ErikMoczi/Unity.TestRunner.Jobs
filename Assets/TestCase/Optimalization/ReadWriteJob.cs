1327a6a92b51bcc54744086615df1bcdc1603847
Erik Moczi <32465761+ErikMoczi@users.noreply.github.com>
2018-08-01 16:36:45 +0200

    Add Optimalization tests

---
+++ Assets/TestCase/Optimalization/ReadWriteJob.cs
@@ -0,0 +1,41 @@
+﻿using TestRunner.Workers;
+using Unity.Collections;
+
+namespace TestCase.Optimalization
+{
+    public struct ReadWriteJob : IJobExt<int, int>
+    {
+        [ReadOnly] private NativeArray<int> _data1;
+        [WriteOnly] private NativeArray<int> _data2;
+
+        public int DataSize { get; set; }
+
+        public NativeArray<int> Data1
+        {
+            get => _data1;
+            set => _data1 = value;
+        }
+
+        public NativeArray<int> Data2
+        {
+            get => _data2;
+            set => _data2 = value;
+        }
+
+        public void Execute()
+        {
+            for (int i = 0; i < DataSize; i++)
+            {
+                _data2[i] = _data1[i];
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public void Init()
+        {
+        }
+    }
+}
---
+++ Assets/WorkSpace/OptimalizationTest.cs
@@ -0,0 +1,72 @@
+﻿using TestCase.Optimalization;
+using TestRunner;
+using TestRunner.Generator;
+using TestRunner.Generator.Interfaces;
+using TestRunner.Wrappers.Base;
+using TestRunner.Wrappers.Base.Config;
+using Unity.Collections;
+
+namespace WorkSpace
+{
+    public class OptimalizationTest : SampleGenerator
+    {
+        private const string TestName = nameof(OptimalizationTest);
+        private const string DataInt1 = "dataInt1";
+
+        protected override ISampleConfig[] InitSampleConfigs()
+        {
+            return new ISampleConfig[]
+            {
+                new SampleConfig(typeof(int), DataInt1)
+            };
+        }
+
+        protected override IWorkWrapper[] InitWorkWrappers()
+        {
+            return new[]
+            {
+                #region UnOptimalized
+
+                WorkerTests<int, int>.RunIJob(TestName, new UnOptimizedJob(), GetData<int>(DataInt1),
+                    GetData<int>(DataInt1), new WorkConfigIJob(Allocator.Persistent)),
+                WorkerTests<int, int>.RunIJobParallelFor(TestName, new UnOptimizedJobParallelFor(),
+                    GetData<int>(DataInt1), GetData<int>(DataInt1),
+                    new WorkConfigIJobParallelFor(Allocator.Persistent)),
+                WorkerTests<int, int>.RunINonJob(TestName, new UnOptimizedNonJob(), GetData<int>(DataInt1),
+                    GetData<int>(DataInt1)),
+
+                #endregion
+
+                #region Optimalized
+
+                WorkerTests<int, int>.RunIJob(TestName, new FullOptimizedJob(), GetData<int>(DataInt1),
+                    GetData<int>(DataInt1), new WorkConfigIJob(Allocator.Persistent)),
+                WorkerTests<int, int>.RunIJob(TestName, new BurstJob(), GetData<int>(DataInt1), GetData<int>(DataInt1),
+                    new WorkConfigIJob(Allocator.Persistent)),
+                WorkerTests<int, int>.RunIJob(TestName, new BurstReadJob(), GetData<int>(DataInt1),
+                    GetData<int>(DataInt1), new WorkConfigIJob(Allocator.Persistent)),
+                WorkerTests<int, int>.RunIJob(TestName, new BurstWriteJob(), GetData<int>(DataInt1),
+                    GetData<int>(DataInt1), new WorkConfigIJob(Allocator.Persistent)),
+                WorkerTests<int, int>.RunIJob(TestName, new ReadWriteJob(), GetData<int>(DataInt1),
+                    GetData<int>(DataInt1), new WorkConfigIJob(Allocator.Persistent)),
+
+                WorkerTests<int, int>.RunIJobParallelFor(TestName, new FullOptimizedJobParallelFor(),
+                    GetData<int>(DataInt1), GetData<int>(DataInt1),
+                    new WorkConfigIJobParallelFor(Allocator.Persistent)),
+                WorkerTests<int, int>.RunIJobParallelFor(TestName, new BurstJobParallelFor(), GetData<int>(DataInt1),
+                    GetData<int>(DataInt1), new WorkConfigIJobParallelFor(Allocator.Persistent)),
+                WorkerTests<int, int>.RunIJobParallelFor(TestName, new BurstReadJobParallelFor(),
+                    GetData<int>(DataInt1), GetData<int>(DataInt1),
+                    new WorkConfigIJobParallelFor(Allocator.Persistent)),
+                WorkerTests<int, int>.RunIJobParallelFor(TestName, new BurstWriteJobParallelFor(),
+                    GetData<int>(DataInt1), GetData<int>(DataInt1),
+                    new WorkConfigIJobParallelFor(Allocator.Persistent)),
+                WorkerTests<int, int>.RunIJobParallelFor(TestName, new ReadWriteJobParallelFor(),
+                    GetData<int>(DataInt1), GetData<int>(DataInt1),
+                    new WorkConfigIJobParallelFor(Allocator.Persistent)),
+
+                #endregion
+            };
+        }
+    }
+}
