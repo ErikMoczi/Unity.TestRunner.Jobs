a6442c2d8b4e731027647af3a410fbff6be95cc1
Erik Moczi <32465761+ErikMoczi@users.noreply.github.com>
2018-08-04 22:22:11 +0200

    Add BasicOptimalization tests

---
+++ Assets/TestCase/Basic/Subtraction/Optimalization/SimpleSubtractionOptimalizationDoubleJobParallelFor.cs
@@ -0,0 +1,47 @@
+﻿using TestRunner.Workers;
+using Unity.Burst;
+using Unity.Collections;
+
+namespace TestCase.Basic.Subtraction.Optimalization
+{
+    [BurstCompile]
+    public struct SimpleSubtractionOptimalizationDoubleJobParallelFor : IJobParallelForExt<double, double, double>
+    {
+        [ReadOnly] private NativeArray<double> _data1;
+        [ReadOnly] private NativeArray<double> _data2;
+        [WriteOnly] private NativeArray<double> _data3;
+
+        public int DataSize { get; set; }
+
+        public NativeArray<double> Data1
+        {
+            get => _data1;
+            set => _data1 = value;
+        }
+
+        public NativeArray<double> Data2
+        {
+            get => _data2;
+            set => _data2 = value;
+        }
+
+        public NativeArray<double> Data3
+        {
+            get => _data3;
+            set => _data3 = value;
+        }
+
+        public void Execute(int i)
+        {
+            _data3[i] = _data1[i] - _data2[i];
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public void Init()
+        {
+        }
+    }
+}
